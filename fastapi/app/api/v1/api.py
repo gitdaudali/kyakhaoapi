e4e885d04ce09aa23e35d834e8a4265bce9b645e
hamzashahzadchattha <talk2chattha@gmail.com>
2025-09-15 01:54:29 +0500

    content router added

--- fastapi/app/api/v1/api.py
+++ fastapi/app/api/v1/api.py
@@ -1,4 +1,4 @@
-from app.api.v1.endpoints import auth, users
+from app.api.v1.endpoints import auth, content, users
 from fastapi import APIRouter
 
 api_router = APIRouter()
@@ -6,3 +6,4 @@ api_router = APIRouter()
 # Include all endpoint routers
 api_router.include_router(auth.router, prefix="/auth", tags=["authentication"])
 api_router.include_router(users.router, prefix="/users", tags=["users"])
+api_router.include_router(content.router, prefix="/content", tags=["content"])
