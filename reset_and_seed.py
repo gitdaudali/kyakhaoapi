ee5643a32cb27f63f98d44d5be6d024d1bde7569
hamzashahzadchattha <talk2chattha@gmail.com>
2025-09-29 22:16:15 +0500

    scripts updated

--- reset_and_seed.py
+++ reset_and_seed.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 """
 Database Reset and Seed Script for Cup Streaming FastAPI Application
 
--- seed_database.py
+++ seed_database.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 """
 Database Seeder for Cup Streaming FastAPI Application
 
